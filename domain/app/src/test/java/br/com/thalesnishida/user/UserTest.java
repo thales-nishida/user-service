0bf913bab977618c43176fdcb05af8f3dfbf7f2b
Thales Nishida <nishida.tn@proton.me>
2025-05-08 09:08:13 -0300

    creting first test diff --git c/domain/app/src/test/java/br/com/thalesnishida/user/UserTest.java i/domain/app/src/test/java/br/com/thalesnishida/user/UserTest.java index 6934998..2df8e8b 100644 --- c/domain/app/src/test/java/br/com/thalesnishida/user/UserTest.java +++ i/domain/app/src/test/java/br/com/thalesnishida/user/UserTest.java @@ -7,13 +7,17 @@ class UserTest {
         @Test public void givenAValidParams_whenCallUser_thenShouldInstantiateUser() {
    - final var name = "Test"; - final var email = "test@test.com.br"; - final var password = "test123"; + final var expectedName = "Test"; + final var expectedEmail = "test@test.com.br"; + final var expectedPassword = "test123";
    
    - final var actualUser = User.newUser(name, email, password); + final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
    
             Assertions.assertNotNull(actualUser); + Assertions.assertNotNull(actualUser.getId()); + Assertions.assertEquals(expectedName, actualUser.getName()); + Assertions.assertEquals(expectedEmail, actualUser.getEmail()); +
    Assertions.assertEquals(expectedPassword, actualUser.getPassword());
    
         }
     }

--- domain/app/src/test/java/br/com/thalesnishida/user/UserTest.java
+++ domain/app/src/test/java/br/com/thalesnishida/user/UserTest.java
@@ -7,13 +7,17 @@ class UserTest {
 
     @Test
     public void givenAValidParams_whenCallUser_thenShouldInstantiateUser() {
-        final var name = "Test";
-        final var email = "test@test.com.br";
-        final var password = "test123";
+        final var expectedName = "Test";
+        final var expectedEmail = "test@test.com.br";
+        final var expectedPassword = "test123";
 
-        final var actualUser = User.newUser(name, email, password);
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
 
         Assertions.assertNotNull(actualUser);
+        Assertions.assertNotNull(actualUser.getId());
+        Assertions.assertEquals(expectedName, actualUser.getName());
+        Assertions.assertEquals(expectedEmail, actualUser.getEmail());
+        Assertions.assertEquals(expectedPassword, actualUser.getPassword());
         
     }
 }
