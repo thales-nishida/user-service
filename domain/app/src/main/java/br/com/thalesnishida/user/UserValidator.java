ea02cfe9a7fd4c3260969d489fa64edd424c4ea6
Thales Nishida <nishida.tn@proton.me>
2025-05-12 09:00:42 -0300

    added test to validate email and password

--- domain/app/src/main/java/br/com/thalesnishida/user/UserValidator.java
+++ domain/app/src/main/java/br/com/thalesnishida/user/UserValidator.java
@@ -1,10 +1,15 @@
 package br.com.thalesnishida.user;
 
+import java.util.regex.Pattern;
+import java.util.regex.MatchResult;
 import br.com.thalesnishida.validations.Validator;
 import br.com.thalesnishida.validations.Error;
 import br.com.thalesnishida.validations.ValidationHandler;
 
 public class UserValidator extends Validator {
+
+    public static final int MIN_LENGTH = 2;
+    public static final int MAX_LENGTH = 255;
     private final User user;
 
     public UserValidator(final User aUser, final ValidationHandler anHandler) {
@@ -15,6 +20,7 @@ public class UserValidator extends Validator {
     @Override
     public void validate() {
         checkName();
+        checkEmail();  
     }
 
     private void checkName() {
@@ -24,6 +30,37 @@ public class UserValidator extends Validator {
             this.validationHandler().append(new Error("'name' should be not null"));
             return;
         }
+
+        if(userName.isEmpty()) {
+            this.validationHandler().append(new Error("'name' should be not empty"));
+            return;
+        }
+
+        if(userName.length() < MIN_LENGTH || userName.length() > MAX_LENGTH) {
+            this.validationHandler().append(new Error("'name' should be between 2 and 255 characters"));
+            return;
+        }
     }
+
+    private void checkEmail() {
+        final var email = this.user.getEmail();
+        final var regexPattern = "^(.+)@(\\S+)$";
+
+        if(email == null) {
+            this.validationHandler().append(new Error("'email' should be not null"));
+            return;
+        }
+
+        if(email.isEmpty()) {
+            this.validationHandler().append(new Error("'email' should be not empty"));
+            return;
+        }
+
+        if(Pattern.compile(regexPattern).matcher(email).find() == false) {
+            this.validationHandler().append(new Error("'email' format invalid"));
+            return;
+        }
+        
+    }  
 
 }
--- domain/app/src/test/java/br/com/thalesnishida/user/UserTest.java
+++ domain/app/src/test/java/br/com/thalesnishida/user/UserTest.java
@@ -28,6 +28,7 @@ class UserTest {
         final var expectedEmail = "test@test.com";
         final var expectedPassword = "test123";
         final var expectedErrorMessage = "'name' should be not null";
+        final var expectedErrorCount = 1;
 
         final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
 
@@ -35,5 +36,214 @@ class UserTest {
             Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
 
         Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAInvalidParamsEmptyName_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "";
+        final var expectedEmail = "test@test.com";
+        final var expectedPassword = "test123";
+        final var expectedErrorMessage = "'name' should be not empty";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAInvalidParamNameLess2Length_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "T";
+        final var expectedEmail = "test@test.com";
+        final var expectedPassword = "test123";
+        final var expectedErrorMessage = "'name' should be between 2 and 255 characters";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAInvalidParamNameMore255Length_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = """
+                cwofydnaikxhzsywekxymfynlesjscwywrjrrpgihitqnogekesncqzyytulcgtjqziilhfqfiucjmyfepnxwojgkubntddaobfiqmka
+                qfkafukodromrunqpsjvxlkzwgsdmqyrpfvfjctcxmjcheokrfbydbsoolqnavovhuveoujiuddgqaakbtwxxpkefwbyfasgypbwdoffyx
+                kulvdvympkeqblfocvhrlpzpxfbbdrkbkegbmhcimwxmyp
+            """;
+        final var expectedEmail = "test@test.com";
+        final var expectedPassword = "test123";
+        final var expectedErrorMessage = "'name' should be between 2 and 255 characters";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+    
+    @Test
+    public void givenAInvalidParamEmailNull_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "Teste";
+        final String expectedEmail = null;
+        final var expectedPassword = "test123";
+        final var expectedErrorMessage = "'email' should be not null";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAInvalidParamEmailEmpty_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "Teste";
+        final var expectedEmail = "";
+        final var expectedPassword = "test123";
+        final var expectedErrorMessage = "'email' should be not empty";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+    
+    @Test
+    public void givenAInvalidParamEmailInvalidFormat_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "Teste";
+        final var expectedEmail = "testtest.com";
+        final var expectedPassword = "test123";
+        final var expectedErrorMessage = "'email' format invalid";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+    
+    @Test
+    public void givenAInvalidParamPasswordNull_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "Teste";
+        final var expectedEmail = "teste@teste.com";
+        final String expectedPassword = null;
+        final var expectedErrorMessage = "'password' should be not null";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+    
+    @Test
+    public void givenAInvalidParamPasswordLess8Length_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "Teste";
+        final var expectedEmail = "teste@teste.com";
+        final var expectedPassword = "920@0";
+        final var expectedErrorMessage = "'password' should be contains at least 8 characters and at most 20 characters";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+    
+    @Test
+    public void givenAInvalidParamPasswordWithNoDigit_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "Teste";
+        final var expectedEmail = "teste@teste.com";
+        final var expectedPassword = "nodigitpass";
+        final var expectedErrorMessage = "'password' should be contains at least one digit";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAInvalidParamPasswordWithoutUpperCaseAlphabet_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "Teste";
+        final var expectedEmail = "teste@teste.com";
+        final var expectedPassword = "nodig2itpass";
+        final var expectedErrorMessage = "'password' should be contains at least one uppercase alphabet";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAInvalidParamPasswordWithoutLowerCaseAlphabet_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "Teste";
+        final var expectedEmail = "teste@teste.com";
+        final var expectedPassword = "TEST1T@TEST";
+        final var expectedErrorMessage = "'password' should be contains at least one lowercase alphabet";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+    
+    @Test
+    public void givenAInvalidParamPasswordNoIncludesSpecialCharacters_whenCallNewUser_thenShouldReturnADomainException() {
+        final var expectedName = "Teste";
+        final var expectedEmail = "teste@teste.com";
+        final var expectedPassword = "TEST1TEST";
+        final var expectedErrorMessage = "'password' should be contains at least one lowercase alphabet";
+        final var expectedErrorCount = 1;
+
+        final var actualUser = User.newUser(expectedName, expectedEmail, expectedPassword);
+
+        final var actualException = 
+            Assertions.assertThrows(DomainException.class, () -> actualUser.validate(new ThrowValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
     }
 }
