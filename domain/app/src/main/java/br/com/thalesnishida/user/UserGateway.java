fe8fc1acab5734abd973701628d42a4215211f13
Thales Nishida <nishida.tn@proton.me>
2025-05-13 08:13:13 -0300

    added update method on UserGateway

---
+++ domain/app/src/main/java/br/com/thalesnishida/user/UserGateway.java
@@ -0,0 +1,10 @@
+package br.com.thalesnishida.user;
+
+import java.util.Optional;
+
+public interface UserGateway {
+    User create(User aUser);
+    void deleteById(UserId anId);
+    Optional<User> findById(UserId anId);
+    User update(User aUser);
+}
