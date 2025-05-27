32f61d3937b91c75c36d6dea7a18967af570c7e8
Thales Nishida <nishida.tn@proton.me>
2025-05-27 08:59:57 -0300

    config database mysql docker

--- infrastructure/src/main/java/br/com/thalesnishida/Main.java
+++ infrastructure/src/main/java/br/com/thalesnishida/Main.java
@@ -1,6 +1,7 @@
 package br.com.thalesnishida.user.service.infrastructure;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import br.com.thalesnishida.user.service.infrastructure.configuration.WebServerConfig;
 
@@ -9,6 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
