76814bba98e6a75fe4c9b5ca0ca60305a938b438
Thales Nishida <nishida.tn@proton.me>
2025-06-09 08:48:19 -0300

    wip

--- infrastructure/src/main/java/br/com/thalesnishida/Main.java
+++ infrastructure/src/main/java/br/com/thalesnishida/Main.java
@@ -9,7 +9,7 @@ import br.com.thalesnishida.user.service.infrastructure.configuration.WebServerC
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
         // System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
